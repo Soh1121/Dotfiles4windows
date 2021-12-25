d21b5f78928bbe98d24ba5e8fc1aeb658eec7b60
Soh1121 <satou.shg@gmail.com>
2021-12-25 18:17:27 +0900

    [update]バーチャルホストの有効

--- make.ps1
+++ make.ps1
@@ -1,3 +1,11 @@
-echo "test"
+# DO NOT RUN ON ISE
+# Set-ExecutionPolicy -Scope Process Unrestricted
+
+# バーチャルホストの有効
+# Linux 用 Windows サブシステム
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+# 仮想マシン プラットフォーム
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 pause
