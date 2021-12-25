6f2ace80fcb2c0ec8df8b6f976949a4f99facc19
Soh1121 <satou.shg@gmail.com>
2021-12-25 18:28:18 +0900

    [add]バーチャルホストの有効化を別ファイルに

---
+++ init.ps1
@@ -0,0 +1,12 @@
+# DO NOT RUN ON ISE
+# Set-ExecutionPolicy -Scope Process Unrestricted
+
+# バーチャルホストの有効
+# Linux 用 Windows サブシステム
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+# 仮想マシン プラットフォーム
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# 再起動
+Restart-Computer Force
--- make.ps1
+++ make.ps1
@@ -1,11 +1,3 @@
-# DO NOT RUN ON ISE
-# Set-ExecutionPolicy -Scope Process Unrestricted
-
-# バーチャルホストの有効
-# Linux 用 Windows サブシステム
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-
-# 仮想マシン プラットフォーム
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+echo "test"
 
 pause
