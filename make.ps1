c1abb490c97c4cd1213c1513550572e5e9ca480a
Soh1121 <satou.shg@gmail.com>
2021-12-25 18:41:38 +0900

    [update]Scoopをインストール

--- make.ps1
+++ make.ps1
@@ -1,3 +1,13 @@
-echo "test"
+# DO NOT RUN ON ISE
+# Set-ExecutionPolicy -Scope Process Unrestricted
+
+# Scoop
+## インストール
+try {
+  Get-Command -Name scoop -ErrorAction Stop
+}
+catch [System.Management.Automation.CommandNotFoundException] {
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
 
 pause
