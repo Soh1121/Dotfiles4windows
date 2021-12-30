a27c9f43510630aabe4d0f81611924ebe5d291e7
Soh1121 <satou.shg@gmail.com>
2021-12-30 18:51:43 +0900

    [add]wsl2をインストールできるように

--- init.ps1
+++ init.ps1
@@ -3,10 +3,10 @@
 
 # バーチャルホストの有効
 # Linux 用 Windows サブシステム
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
 # 仮想マシン プラットフォーム
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 # 再起動
 Restart-Computer -Force
---
+++ wsl2.ps1
@@ -0,0 +1,12 @@
+# WSL 2 Kernel Update
+Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
+msiexec /i wsl_update_x64.msi /passive /norestart
+rm wsl_update_x64.msi
+
+# WSL 2 を標準にする
+wsl --set-default-version 2
+
+# Ubuntu 2004 をインストール
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile linux.appx -UseBasicParsing
+Add-AppxPackage -Path linux.appx
+rm linux.appx
