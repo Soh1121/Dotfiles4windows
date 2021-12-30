f3751bac30cfbe5d3fd6adb74b3ee92d4cba4645
Soh1121 <satou.shg@gmail.com>
2021-12-30 20:50:12 +0900

    [update]インストール部分をプロセス化

--- getWsl2.bat
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
-Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile linux.appx -UseBasicParsing
--- wsl2.ps1
+++ wsl2.ps1
@@ -1,12 +1,13 @@
 # WSL 2 Kernel Update
-Start-Process getWsl2.bat -Wait
-msiexec /i wsl_update_x64.msi /passive /norestart
+Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
+Start-Process msiexec /i wsl_update_x64.msi /passive /norestart -Wait
 rm wsl_update_x64.msi
 
 # WSL 2 を標準にする
 wsl --set-default-version 2
 
 # Ubuntu 2004 をインストール
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile linux.appx -UseBasicParsing
 Add-AppxPackage -Path linux.appx
 rm linux.appx
 
