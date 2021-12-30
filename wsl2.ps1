3eb9f78aaab80d10cfe657989e8bb55e5bee099a
Soh1121 <satou.shg@gmail.com>
2021-12-30 20:55:39 +0900

    [fix]メソッドを変更

--- wsl2.ps1
+++ wsl2.ps1
@@ -1,13 +1,14 @@
 # WSL 2 Kernel Update
-Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
-Start-Process msiexec /i wsl_update_x64.msi /passive /norestart -Wait
+# Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
+Invoke-RestMethod https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi
+msiexec /i wsl_update_x64.msi /passive /norestart
 rm wsl_update_x64.msi
 
 # WSL 2 を標準にする
 wsl --set-default-version 2
 
 # Ubuntu 2004 をインストール
-Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile linux.appx -UseBasicParsing
+Invoke-RestMethod https://aka.ms/wslubuntu2004 -OutFile linux.appx
 Add-AppxPackage -Path linux.appx
 rm linux.appx
 
